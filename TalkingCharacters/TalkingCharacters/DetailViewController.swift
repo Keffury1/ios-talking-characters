1c756571a044218f4dd47b7e61b97f6392c7d00c
Robert Keffury <bobby.keffury@me.com>
2019-08-01 19:35:06 -0700

    Completed the Storyboard aspects.

---
+++ TalkingCharacters/TalkingCharacters/DetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DetailViewController.swift
+//  TalkingCharacters
+//
+//  Created by Bobby Keffury on 8/1/19.
+//  Copyright © 2019 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    
+    @IBOutlet weak var talkingLabel: UILabel!
+    @IBOutlet weak var imageView2: UIImageView!
+    
+    var character: String?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+   
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
